f5eb946231683da2e887cd34cffe748fa2656e29
Ryan4545 <51761134+Ryan4545@users.noreply.github.com>
2019-10-28 10:42:03 -0400

    Added main content

---
+++ dist/js/main.js
@@ -0,0 +1,11 @@
+function openSide() {
+    document.getElementById('side-menu').style.width = '250px';
+}
+
+function closeSide() {
+    document.getElementById('side-menu').style.width = '0px';
+}
+
+function social() {
+    window.location.replace('https://www.instagram.com/4541cavineers/?hl=en');
+}
