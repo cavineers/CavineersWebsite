5dd533e24cb77d1db334df26c3f42d1640e639b1
Ryan4545 <51761134+Ryan4545@users.noreply.github.com>
2019-10-31 19:40:24 -0400

    Changed App

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,3 +9,55 @@ function closeSide() {
 function social() {
     window.location.replace('https://www.instagram.com/4541cavineers/?hl=en');
 }
+
+// function search() {
+//     var x = document.getElementById("inputTag");
+//     var valueOfBox = document.getElementById("inputTag").value;
+//     if (valueOfBox == "Outreach" || valueOfBox == "outreach" || valueOfBox == "out") {
+//         document.getElementById("searchLink").innerHTML = "Hello";
+//     } else {
+//         console.log("no");
+//     }
+// }
+
+function getPass() {
+    window.location.replace('admin.html')
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(xhttp.responseText);
+            var pass = response.pass;
+            var password = document.getElementById("password").value;
+            for (var i = 0; i < pass.length; i++) {
+                if (password == pass[i].ps) {
+                    window.location.replace("admin.html");
+                } else {
+                    console.log('Login in');
+                }
+            }
+        }
+    };
+    xhttp.open("GET", "users.json", true);
+    xhttp.send();
+}
+
+
+function getUser() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(xhttp.responseText);
+            var users = response.users;
+            var username = document.getElementById("username").value;
+            for (var i = 0; i < users.length; i++) {
+                if (username == users[i].user) {
+                    getPass();
+                } else {
+                    console.log('Login in');
+                }
+            }
+        }
+    };
+    xhttp.open("GET", "users.json", true);
+    xhttp.send();
+}
