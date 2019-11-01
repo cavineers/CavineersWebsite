42fafb7be594135f6890fdb16bbeeca4e81fdec0
Ryan4545 <51761134+Ryan4545@users.noreply.github.com>
2019-11-01 16:04:29 -0400

    Changed dev server settings

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,7 +9,6 @@ function closeSide() {
 function social() {
     window.location.replace('https://www.instagram.com/4541cavineers/?hl=en');
 }
-
 // function search() {
 //     var x = document.getElementById("inputTag");
 //     var valueOfBox = document.getElementById("inputTag").value;
@@ -60,4 +59,26 @@ function getUser() {
     };
     xhttp.open("GET", "js/users.json", true);
     xhttp.send();
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
